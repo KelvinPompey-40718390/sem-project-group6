d48fd8c2de01bbbff37546fad2386fb8972975ee
KelvinPompey-40718390 <40718390@live.napier.ac.uk>
2024-02-15 09:34:48 -0400

    Changed database password and upgraded MySQL connector version to fix a vulnerability in version 18

--- src/main/java/com/napier/seprojectgroup6/App.java
+++ src/main/java/com/napier/seprojectgroup6/App.java
@@ -36,7 +36,7 @@ public class App {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "semgroup6");
                 System.out.println("Successfully connected");
                 break;
             }
