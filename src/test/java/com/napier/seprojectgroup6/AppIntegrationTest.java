f01b72835a8af5102a259bad0aec0ab1360c6f3b
KelvinPompey-40718390 <40718390@live.napier.ac.uk>
2024-03-18 10:56:49 -0400

    Add exclude filter for maven build

--- src/test/java/com/napier/seprojectgroup6/AppIntegrationTest.java
+++ src/test/java/com/napier/seprojectgroup6/AppIntegrationTest.java
@@ -1,11 +1,7 @@
-package com.napier.sem;
+package com.napier.seprojectgroup6;
 
-import com.napier.seprojectgroup6.App;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
