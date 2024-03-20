1c87820eba207c2ce4bf2a0923bd150b0d810ebd
Anton Jardine <40690620@live.napier.ac.uk>
2024-03-19 20:13:03 -0400

    Added Capital City and Country Classes for Report Structure.

---
+++ src/main/java/com/napier/seprojectgroup6/db/CapitalCity.java
@@ -0,0 +1,10 @@
+package com.napier.seprojectgroup6.db;
+
+// Field names for Report Display
+public class CapitalCity {
+
+    public String name;
+    public String country;
+    public int population;
+
+}
---
+++ src/main/java/com/napier/seprojectgroup6/db/Country.java
@@ -0,0 +1,11 @@
+package com.napier.seprojectgroup6.db;
+
+// Field names for Report Display
+public class Country {
+    public String code;
+    public String name;
+    public String continent;
+    public String region;
+    public int population;
+    public String capital;
+}
