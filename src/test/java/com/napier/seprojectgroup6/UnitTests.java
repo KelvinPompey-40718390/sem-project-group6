127d25781e9e5deb8b95ad33cc08af1d5babbdb0
Anton Jardine <40690620@live.napier.ac.uk>
2024-04-16 19:43:05 -0400

    Setup Structure for Unit Tests

---
+++ src/test/java/com/napier/seprojectgroup6/UnitTests.java
@@ -0,0 +1,29 @@
+package com.napier.seprojectgroup6;
+
+import com.napier.seprojectgroup6.navigation.*;
+import com.napier.seprojectgroup6.navigation.ReportMenu;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UnitTests
+{
+    static ReportMenu reportMenu;
+
+    @BeforeAll
+    static void init()
+    {
+        reportMenu = new ReportMenu();
+    }
+
+    @Test
+    void reportMenuTest()
+    {
+        reportMenu.
+    }
+
+}
