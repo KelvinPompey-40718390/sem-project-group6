d99fc19fc81ef9cd5482d89930e96f5e66502425
Anton Jardine <40690620@live.napier.ac.uk>
2024-04-15 20:44:55 -0400

    Removed 20 Populated Capital Cities in the World from Navigator

--- src/main/java/com/napier/seprojectgroup6/navigation/Navigator.java
+++ src/main/java/com/napier/seprojectgroup6/navigation/Navigator.java
@@ -28,7 +28,6 @@ public class Navigator {
             new ReportMenu( "14 Top Populated Cities by Region", TopPopulatedCitiesByRegionReport.class.getName()),
             new ReportMenu( "15 Top Populated Cities By Country", TopPopulatedCitiesByCountryReport.class.getName()),
             new ReportMenu( "16 Populated Capital Cities by District", PopulatedCapitalCitiesByDistrict.class.getName()),
-            new ReportMenu( "20 Populated Capital Cities in the World", PopulatedCapitalCitiesinWorld.class.getName()),
             new ReportMenu( "21 Populated Capital Cities by Continent", PopulatedCapitalCitiesByContinent.class.getName()),
             new ReportMenu( "22 Populated Capital Cities by Region", PopulatedCapitalCitiesByRegion.class.getName()),
             new ReportMenu( "23 Percent of Population Living in/out of city by Continent", PopulationInEachContinent.class.getName()),
