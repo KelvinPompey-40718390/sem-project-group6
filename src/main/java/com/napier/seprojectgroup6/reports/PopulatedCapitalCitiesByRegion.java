c83f89642c2bf7ee5c803d18d8b78025e0b8036c
Anton Jardine <40690620@live.napier.ac.uk>
2024-04-16 19:06:57 -0400

    Added comments.

--- src/main/java/com/napier/seprojectgroup6/reports/PopulatedCapitalCitiesByRegion.java
+++ src/main/java/com/napier/seprojectgroup6/reports/PopulatedCapitalCitiesByRegion.java
@@ -48,6 +48,7 @@ public class PopulatedCapitalCitiesByRegion implements Report {
         return Utils.readInput("Enter number of Capital cities to display, or 0 to show all");
     }
 
+    // Ask User to Enter the Region
     private String getRegion()
     {
         return Utils.readInput("Enter Name of Region");
@@ -108,6 +109,7 @@ public class PopulatedCapitalCitiesByRegion implements Report {
         }
     }
 
+    // Insert Header Rows in Preparation to Display Query results
     public void displayCapitalCities() {
         if(this.capitalCities == null) {
             return;
@@ -120,6 +122,7 @@ public class PopulatedCapitalCitiesByRegion implements Report {
         }
     }
 
+    // Display results of Query
     private void displayCapitalCity(CapitalCity capitalCity) {
         if(capitalCity != null) {
             System.out.printf("%-30s %-40s %-10s \n",  capitalCity.name, capitalCity.country, capitalCity.population);
