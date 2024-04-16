05e5e95afedecb332099adda0338d2c4f5b2279a
Anton Jardine <40690620@live.napier.ac.uk>
2024-04-15 21:02:19 -0400

    Removed Duplicate 14 Top Populated Cities by Region from Navigator Menu

--- src/main/java/com/napier/seprojectgroup6/navigation/Navigator.java
+++ src/main/java/com/napier/seprojectgroup6/navigation/Navigator.java
@@ -10,7 +10,6 @@ public class Navigator {
             new ReportMenu( "11 Cities By District", CitiesByDistrictReport.class.getName()),
             new ReportMenu( "12 Top Cities by Population", TopPopulatedCitiesReport.class.getName()),
             new ReportMenu("13 Top Populated Cities by Continent", TopPopulatedCitiesByContinentReport.class.getName()),
-            new ReportMenu( "14 Top Populated Cities by Region", TopPopulatedCitiesByRegionReport.class.getName()),
             new ReportMenu("01 Countries In World", CountriesInWorld.class.getName()),
             new ReportMenu("03 Countries In A Region By Population Size", CountriesInARegionByPopulationSize.class.getName()),
             new ReportMenu("04 Top Populated Countries", TopPopulatedCountries.class.getName()),
