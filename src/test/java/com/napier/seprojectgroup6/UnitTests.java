4de2756074f0a05d60bbb5f87f4f7054348c1244
kelvinpompey <40718390@live.napier.ac.uk>
2024-04-16 20:18:40 -0400

    Made ReportMenu constructor and fields public to accomodate unit tests

--- src/test/java/com/napier/seprojectgroup6/UnitTests.java
+++ src/test/java/com/napier/seprojectgroup6/UnitTests.java
@@ -1,7 +1,10 @@
 package com.napier.seprojectgroup6;
 
+import com.napier.seprojectgroup6.db.Population;
 import com.napier.seprojectgroup6.navigation.*;
 import com.napier.seprojectgroup6.navigation.ReportMenu;
+import com.napier.seprojectgroup6.reports.CitiesByDistrictReport;
+import com.napier.seprojectgroup6.reports.PopulationOfACity;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -13,17 +16,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UnitTests
 {
     static ReportMenu reportMenu;
+    static CitiesByDistrictReport citiesByDistrictReport;
 
     @BeforeAll
     static void init()
     {
-        reportMenu = new ReportMenu();
+        reportMenu = new ReportMenu("Population of City Report", PopulationOfACity.class.getName());
+        citiesByDistrictReport = new CitiesByDistrictReport();
     }
 
     @Test
-    void reportMenuTest()
+    void reportMenuTestTitle()
     {
-        reportMenu.
+        assertEquals(reportMenu.title, "Population of City Report");
     }
 
+    @Test
+    void reportMenuTestClass()
+    {
+        assertEquals(reportMenu.className, "PopulationOfACity");
+    }
 }
