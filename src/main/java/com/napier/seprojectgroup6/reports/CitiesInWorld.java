92e948fc4343cb85748b768f5eb4386a6a7bab35
Bobby Walters <40669542@live.napier.ac.uk>
2024-04-18 05:37:13 -0400

    Added Input for Total Cities or Limit on Cities.

--- src/main/java/com/napier/seprojectgroup6/reports/CitiesInWorld.java
+++ src/main/java/com/napier/seprojectgroup6/reports/CitiesInWorld.java
@@ -15,7 +15,9 @@ public class CitiesInWorld implements Report {
     private Connection con = null;
     public ArrayList<City> cities;
     public ArrayList<Country> countries;
-   public CitiesInWorld() {
+    private Integer limit;
+    public String city;
+    public CitiesInWorld() {
         this.con = ConnectionManager.getInstance().getConnection();
     }
 
@@ -24,29 +26,59 @@ public class CitiesInWorld implements Report {
      * execute the query
      */
     public void run() {
+        city = this.getCity();
+        limit = Integer.parseInt(this.getInput());
         this.executeQuery();
         this.displayCities();
     }
 
-    public void runWithCities() {
+    public void runWithInputs(Integer limit, String city) {
+        this.city = city;
+        this.limit = limit;
         this.executeQuery();
         this.displayCities();
     }
 
-       private void executeQuery()
+    private String getInput() {
+        return Utils.readInput("Enter Number of Cities to display, or 0 to Show All");
+    }
+
+    private String getCity()
+    {
+        return Utils.readInput("Enter Name of City");
+    }
+    private void executeQuery()
     {
         cities = new ArrayList<>();
-try
-               {
+        if(this.limit == null) {
+            return;
+        }
+        try
+        {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
-            String strSelect =
-                    "SELECT city.Name,country.Name AS CountryName,  city.District, city.Population\n" +
-                            "FROM city\n" +
-                            "         INNER JOIN country ON city.CountryCode = country.Code\n" +
-                            "WHERE country.Continent\n" +
-                            "ORDER BY city.Population DESC ";
+            String strSelect = "";
+
+
+            if(this.limit > 0) {
+                strSelect = "SELECT city.Name,country.Name AS CountryName,  city.District, city.Population\n" +
+                        "FROM city\n" +
+                        "INNER JOIN country ON city.CountryCode = country.Code\n" +
+                        "WHERE country.Continent\n" +
+                        "ORDER BY city.Population Desc " +
+                        "LIMIT " + this.limit;
+            }
+
+            else {
+                strSelect =     "SELECT city.Name,country.Name AS CountryName,  city.District, city.Population\n" +
+                        "FROM city\n" +
+                        "INNER JOIN country ON city.CountryCode = country.Code\n" +
+                        "WHERE country.Continent\n" +
+                        "ORDER BY city.Population Desc ";
+            }
+
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
 
@@ -77,8 +109,8 @@ try
             return;
         }
 
-        System.out.println("All the Cities In the World: ");
-        System.out.printf("%-35s %-40s %-30s %-15s\n",  "City", "Country", "District", "Population");
+        System.out.println("Top N and All Cities In the World: ");
+        System.out.printf("%-35s %-40s %-30s %-15s\n",  "CITY", "COUNTRY", "DISTRICT", "POPULATION");
         for(City city: cities) {
             this.displayCity(city);
         }
